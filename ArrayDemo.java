ed1f46eccb5abec469d6bf41dbd2a6371e46529a
Justin Chen <jchen06@stuy.edu>
2018-09-27 17:01:32 -0400

    Halfway done, will finish

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -63,15 +63,27 @@ public class ArrayDemo {
     return poi;
 }
 
-
-
   //Main
   public static void main(String[] args) {
-      //int [] xyz = new int[5];
-      //xyz[0] = 1;
-      // printArray(xyz);
-      //int [] arr = new int[] {1,2,4};
-      //printArray(arr);
+    System.out.println("0a. Prints a 1D array of ints");
+      int [] xyz = new int[5];
+      xyz[0] = 0;
+      xyz[1] = 2;
+      xyz[2] = -2;
+      xyz[3] = 4;
+      xyz[4] = -4;
+      printArray(xyz);
+
+    System.out.println("0b. Prints a 2D array of ints");
+      int [][] lol = new int[3][5];
+      printArray(lol);
+
+    System.out.println("1. Returns amount of zeroes in a parameter (5x5 array)");
+      int [][] heck = new int [5][5];
+      System.out.println(countZeros2D(heck));
+
+    System.out.println("2a. Modifies a 2D array; all values replaced with 1 unless row number = column, in that case, replaced with 3");
+
 /*
       int [][] abc = new int[3][];
       abc[0] = new int [3];
@@ -79,12 +91,14 @@ public class ArrayDemo {
       abc[2] = new int [5];
       fill2D(abc);
       printArray(abc);
-*/
+
+
       int [] [] bnm = new int [] [] {
         {-5,3,5},
         {23,45,62},
         {-5,-10,0},
       };
       printArray(fill2DCopy(bnm));
+*/
 }
 }
