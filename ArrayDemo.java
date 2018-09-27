991fe621f7a4df6f1c421f83ca96552cb2038bdd
Justin Chen <jchen06@stuy.edu>
2018-09-27 00:30:45 -0400

    Completed 1, started on 2a

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -15,4 +15,42 @@ public class ArrayDemo {
       System.out.println("");
     }
 }
+
+  // 1
+  public static int countZeros2D(int[][] nums) {
+    int ret = 0;
+    for (int s = 0; s < nums.length; s++) {
+      for (int t = 0; t < nums[s].length; t++) {
+        if (nums[s][t] == 0) {
+          ret = ret + 1;
+        }
+      }
+    }
+    return ret;
+}
+
+  //2a THIS DOES NOT WORK YET
+  public static void fill2D(int[][] vals) {
+    int row = 0;
+    int column = 0;
+    int placeholder = 0;
+    for (int u = 0; u < vals.length; u++) {
+      row = vals[u];
+      for (int v = 0; v < vals[u].length; v++) {
+        column = vals[u][v];
+        if (column == row) {
+          placeholder = 1;
+          vals[u][v] = 3;
+        } else {
+          vals[u][v] = 1;
+        }
+      }
+      if (placeholder == 1) {
+        vals[u] = 3;
+      } else {
+        vals[u] = 1;
+      }
+    }
+    return vals;
+  }
 }
