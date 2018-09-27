8e2838c7c070e265afc6aeb070753bffb8c022af
Justin Chen <jchen06@stuy.edu>
2018-09-26 23:45:13 -0400

    Completed 0a and 0b

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,1 +1,18 @@
+public class ArrayDemo {
+  // 0a
+  public static void printArray(int[]ary) {
+    for (int x = 0; x < ary.length; x++) {
+      System.out.print("[" + ary[x] + "]");
+    }
+  }
 
+  // 0b
+  public static void printArray(int[][]ary) {
+    for (int a = 0; a < ary.length; a++) {
+      for (int b = 0; b < ary[a].length; b++) {
+        System.out.print("[" +ary[a][b] + "]");
+      }
+      System.out.println("");
+    }
+}
+}
